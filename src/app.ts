37678aba4df734e75a6bdf528f51d38956c76053
Moisés Santana <moisaant@gmail.com>
2023-11-26 17:05:30 -0300

    feat: add prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
 import fastify from 'fastify';
 
+import { PrismaClient } from '@prisma/client';
+
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    email: 'email@email.com',
+    name: 'nome',
+  }
+});
