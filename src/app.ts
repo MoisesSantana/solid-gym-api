7d7b80cff3ab9d4b77d67749ebbb9839eac50665
Moisés Santana <moisaant@gmail.com>
2023-11-26 18:05:56 -0300

    feat: create relationships

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,3 @@
 import fastify from 'fastify';
 
-import { PrismaClient } from '@prisma/client';
-
 export const app = fastify();
-
-const prisma = new PrismaClient();
-
-prisma.user.create({
-  data: {
-    email: 'email@email.com',
-    name: 'nome',
-  }
-});
