cbcb32758b14ec76c27df0a82ab5ea9861f90b5f
Moisés Santana <moisaant@gmail.com>
2023-11-28 19:47:06 -0300

    test: check-in

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,7 +1,7 @@
 
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository';
 
-import { beforeEach, describe, expect, it } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 
 import { CheckInUseCase } from './check-in';
 
@@ -12,14 +12,36 @@ describe('Check In Use Case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository();
     sut = new CheckInUseCase(checkInsRepository);
+
+    vi.useFakeTimers();
   });
 
-  it('should not be able to check in', async () => {
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  it('should be able to check in', async () => {
     const { checkIn } = await sut.execute({
       gymId: 'gym-id',
       userId: 'user-id',
     });
 
     expect(checkIn.id).toEqual(expect.any(String));
+  });
+
+  it('should not be able to check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(1994, 5, 5, 8, 0, 0));
+
+    await sut.execute({
+      gymId: 'gym-id',
+      userId: 'user-id',
+    });
+
+    await expect(() => {
+      sut.execute({
+        gymId: 'gym-id',
+        userId: 'user-id',
+      });
+    }).rejects.toBeInstanceOf(Error);
   });
 });
