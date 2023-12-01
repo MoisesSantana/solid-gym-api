f96fe7f354b733d741db66e65f983f9ee2b35fa7
Moisés Santana <moisaant@gmail.com>
2023-12-01 01:32:43 -0300

    feat: not be able to check in on distant gym

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -2,6 +2,7 @@ import { CheckIn } from '@prisma/client';
 
 import { CheckInsRepository } from '@/repositories/check-ins-repository';
 import { GymsRepository } from '@/repositories/gyms-repository';
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates';
 
 import { ResourceNotFoundError } from './errors/resource-not-found-error';
 
@@ -25,10 +26,21 @@ export class CheckInUseCase {
   async execute({
     userId,
     gymId,
+    userLatitude,
+    userLongitude,
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
     const gym = await this.gymsRepository.findById(gymId);
 
     if (!gym) throw new ResourceNotFoundError();
+
+    const distance = getDistanceBetweenCoordinates(
+      { latitude: userLatitude, longitude: userLongitude },
+      { latitude: +gym.latitude, longitude: +gym.longitude },
+    );
+
+    const MAX_DISTANCE = 0.1;
+
+    if (distance > MAX_DISTANCE) throw new Error();
 
     const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
       userId,
