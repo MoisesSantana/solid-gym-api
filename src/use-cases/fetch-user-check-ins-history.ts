f203b113813b3485f481447d9a8a49cf145907d7
Moisés Santana <moisaant@gmail.com>
2023-12-01 02:38:34 -0300

    feat: fetch paginated check-in history

---
+++ src/use-cases/fetch-user-check-ins-history.ts
@@ -0,0 +1,25 @@
+import { CheckIn } from '@prisma/client';
+
+import { CheckInsRepository } from '@/repositories/check-ins-repository';
+
+interface FetchUserCheckInsHistoryRequest {
+  userId: string;
+  page: number;
+}
+
+interface FetchUserCheckInsHistoryResponse {
+  checkIns: CheckIn[];
+}
+
+export class FetchUserCheckInsHistoryUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    page,
+  }: FetchUserCheckInsHistoryRequest): Promise<FetchUserCheckInsHistoryResponse> {
+    const checkIns = await this.checkInsRepository.findManyByUserId(userId, page);
+
+    return { checkIns };
+  }
+}
