e37703dc876138669594a1ed6393f87fce7ce2ea
Moisés Santana <moisaant@gmail.com>
2023-12-05 03:44:30 -0300

    feat: add jwt

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import fastify from 'fastify';
 
+import fastifyJwt from '@fastify/jwt';
+
 import { ZodError } from 'zod';
 
 import { env } from './env';
 import { appRoutes } from './http/routes';
 
 export const app = fastify();
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+});
 
 app.register(appRoutes);
 
