116a0e5e910aca0040261afb0d2d2a04a1191081
Moisés Santana <moisaant@gmail.com>
2023-12-05 02:55:20 -0300

    feat: factory pattern

--- src/use-cases/factories/make-auth-use-case.ts
+++ src/use-cases/factories/make-auth-use-case.ts
@@ -4,7 +4,7 @@ import { AuthUseCase } from '../auth';
 
 export function makeAuthUseCase() {
   const usersRepository = new PrismaUsersRepository();
-  const authUseCase = new AuthUseCase(usersRepository);
+  const useCase = new AuthUseCase(usersRepository);
 
-  return authUseCase;
+  return useCase;
 }
---
+++ src/use-cases/factories/make-create-gym-use-case.ts
@@ -0,0 +1,12 @@
+
+import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository';
+
+import { CreateGymUseCase } from '../create-gym';
+
+export function makeCreateGymUseCase() {
+  const gymsRepository = new PrismaGymsRepository();
+
+  const useCase = new CreateGymUseCase(gymsRepository);
+
+  return useCase;
+}
