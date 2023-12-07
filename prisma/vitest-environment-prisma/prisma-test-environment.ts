4580f6a972ec3c412373769f67b7d3d93aa9da8d
Moisés Santana <moisaant@gmail.com>
2023-12-06 22:02:47 -0300

    chore: config e2e environment tests

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,12 +1,41 @@
+import { PrismaClient } from '@prisma/client';
+
+import 'dotenv/config';
+import { execSync } from 'node:child_process';
+import { randomUUID } from 'node:crypto';
 import { Environment } from 'vitest';
+
+const prisma = new PrismaClient();
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL)
+    throw new Error('❌ Please provide a DATABASE_URL environment variable');
+  
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set('schema', schema);
+
+  return String(url);
+}
 
 export default <Environment> {
   name: 'prisma',
   setup: async () => {
-    console.log('testando environment');
+    const schema = randomUUID();
+    const database = generateDatabaseURL(schema);
+
+    process.env.DATABASE_URL = database;
+
+    execSync('npx prisma migrate deploy');
     
     return {
-      teardown() {}
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        );
+
+        await prisma.$disconnect();
+      }
     };
   },
   transformMode: 'ssr',
