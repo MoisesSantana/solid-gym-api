2babe83c1b40b7f53cb89766d68bed59be3da2a7
Moisés Santana <moisaant@gmail.com>
2023-12-09 00:44:36 -0300

    test: register e2e

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,28 @@
-import { test } from 'vitest';
+import request from 'supertest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
-test('ok', () => {
+import { app } from '../../app';
 
+const user = {
+  email: 'email@email.com',
+  name: 'Name',
+  password: 'password',
+};
+
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to register', async () => {
+    const response = await request(app.server)
+      .post('/users')
+      .send(user);
+
+    expect(response.status).toBe(201);
+  });
 });
